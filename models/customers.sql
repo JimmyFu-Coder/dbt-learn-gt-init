251122482c032503c18a03aba46749a6688aef1c
jimmy <jinshuaifu@gmail.com>
2026-08-07 07:47:02 +0000

    set source yaml and update the model sql

--- models/customers.sql
+++ models/customers.sql
@@ -5,7 +5,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.customers
+    from {{ source('jaffle_shop', 'customers') }}
 
 ),
 
@@ -17,7 +17,7 @@ orders as (
         order_date,
         status
 
-    from raw.orders
+    from {{ source('jaffle_shop', 'orders') }}
 
 ),
 
